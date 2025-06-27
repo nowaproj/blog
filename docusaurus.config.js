22e4890301480e0e1f15d18246aa3d61ce6c8b91
anassalhi <53692841+anassalhi@users.noreply.github.com>
2025-06-27 19:49:31 +0200

    remove dublication

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,16 +83,6 @@ const config = {
         enableInDevelopment: false, // optional
       },
     ],
-    // Add sitemap plugin
-    [
-      '@docusaurus/plugin-sitemap',
-      {
-        changefreq: 'weekly',
-        priority: 0.5,
-        ignorePatterns: ['/tags/**'],
-        filename: 'sitemap.xml',
-      },
-    ],
   ],
 
   presets: [
